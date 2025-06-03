eac1ef17f94de76a03d730d4261c14da6de704bc
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-06-03 15:56:55 -0500

    Finished JavaBasics Practice 2

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//Prompt user for their age:
+        System.out.println("Enter your age");
+        int userAge = scanner.nextInt();
+
+
+// Calculate how many years they have left to 100.
+int tillOneHundred = (100) - userAge;
+
+//Return how many years the user has till 100
+        System.out.println("You have "+ tillOneHundred +" years till you are 100 years old.");
+
         }
     }
-}
